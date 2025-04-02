6be4c97693b34a869db8a8f85d9892ad9d2c2fb1
melkie yilk <melkieyilk@gmail.com>
2025-04-02 14:48:46 +0300

    make the screens of homepage and products page

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+
+const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const {data} = await axios.get("/api/products");
+        setProducts(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+  if (!products || products.length === 0) {
+    return <div>No products found</div>;
+  }
+
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
