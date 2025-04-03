98344ccf364faa149dddfb3b7bf714c662211466
melkie yilk <melkieyilk@gmail.com>
2025-04-03 19:49:00 +0300

    remove the products route into its route folder for simplicity

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from "express";
-
 import dotenv from "dotenv";
+
+import ProductRouter from "./routes/ProductRoutes.js";
+
 dotenv.config();
 
 import connectDb from "./config/db.js";
-import products from "./data/products.js";
 
 const PORT = process.env.PORT || 5000;
 
@@ -18,18 +19,7 @@ app.get("/", (_, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (_, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404).json({ message: "Product not found" });
-  }
-});
+app.use("/api/products", ProductRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
