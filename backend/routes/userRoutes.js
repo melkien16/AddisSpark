b950482e53bdbe8f529b932e0cda99bbfdbc6fd3
melkie yilk <melkieyilk@gmail.com>
2025-04-05 22:56:09 +0300

    use the custome route and admin middlewares

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,21 +10,21 @@ import {
   deleteUser,
   updateUser,
 } from "../controllers/userController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(registerUser).get(getUsers);
-router.post("/login", authUser);
+router.route("/").post(registerUser).get(protect, admin, getUsers);
+router.post("/auth", authUser);
 router.post("/logout", logoutUser);
 router
   .route("/profile")
-  .get(getUserProfile)
-  .put(updateUserProfile);
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 router
   .route("/:id")
-  .delete(deleteUser)
-  .get(getUserById)
-  .put(updateUser);
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
-
