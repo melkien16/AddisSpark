9ec3c6ca366c3175589e163ab79bb99ac1b451a5
melkie yilk <melkieyilk@gmail.com>
2025-04-21 16:09:55 +0300

    import the paymentScreen into the index file and implement it in protected or private route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,6 +20,7 @@ import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -28,9 +29,10 @@ const router = createBrowserRouter(
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
-      <Route path="/register" element={<RegisterScreen />} /> 
-      <Route path='' element={<PrivateRoute />}>
+      <Route path="/register" element={<RegisterScreen />} />
+      <Route path="" element={<PrivateRoute />}>
         <Route path="/shipping" element={<ShippingScreen />} />
+        <Route path="/payment" element={<PaymentScreen />} />
       </Route>
     </Route>
   )
