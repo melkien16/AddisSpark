03a98b2a4b8acb264f8c084d9f18313984b6a2ef
melkie yilk <melkieyilk@gmail.com>
2025-04-22 10:08:52 +0300

    implement to fetch orders by id

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -52,7 +52,16 @@ const getMyOrders = asyncHandler(async (req, res) => {
 // @route   GET /api/orders/:id
 // @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
-  res.send("get order by id");
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 // @desc    Update order to paid
