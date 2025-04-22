4ac63ab7f8a574ff52bb797909551e71d576f592
melkie yilk <melkieyilk@gmail.com>
2025-04-22 10:02:36 +0300

    modify the orders route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -14,7 +14,7 @@ const router = express.Router();
 router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
 router.route('/mine').get(protect, getMyOrders)
 router.route('/:id').get(protect, admin, getOrderById)
-router.route('/:id/pay').get(protect, admin, updateOrderToPaid)
-router.route('/:id/deliver').get(protect, admin, updateOrderToDelivered)
+router.route('/:id/pay').put(protect, updateOrderToPaid)
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered)
 
 export default router;
