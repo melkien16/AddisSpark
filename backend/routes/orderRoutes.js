7c73491679e26760c8b720ba23976f72790074c7
melkie yilk <melkieyilk@gmail.com>
2025-04-23 23:43:49 +0300

    simple adjustment of the route file

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -12,9 +12,13 @@ import { protect, admin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
-router.route('/mine').get(protect, getMyOrders)
-router.route('/:id').get(protect, admin, getOrderById)
-router.route('/:id/pay').put(protect, updateOrderToPaid)
-router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered)
+
+router.route("/mine").get(protect, getMyOrders);
+
+router.route("/:id").get(protect, admin, getOrderById);
+
+router.route("/:id/pay").put(protect, updateOrderToPaid);
+
+router.route("/:id/deliver").put(protect, admin, updateOrderToDelivered);
 
 export default router;
